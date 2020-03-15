ea0f724c69e79d71c3d7ce1ac7243be122ef06c3
Jack Lawless <Lawless.jackm@gmail.com>
2020-03-14 19:14:35 -0500

    devour burgers still not working

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('smash burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese burger', false);
